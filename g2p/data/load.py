5eb7a662648accc9770d0cdb956ecee54a5a68fa
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-09-29 20:12:38 +0900

    Add load.py

---
+++ g2p/data/load.py
@@ -0,0 +1,13 @@
+from torch.nn.utils.rnn import pack_sequence
+from torch.utils.data import DataLoader
+
+def _pack_data_and_label(batch):
+    data, label = zip(*batch)
+    return pack_sequence(data, enforce_sorted=False), \
+           pack_sequence(label, enforce_sorted=False)
+
+def get_loader(data, batch_size, use_cuda):
+    kwargs = {'dataset': data, 'batch_size': batch_size,
+              'collate_fn': _pack_data_and_label}
+    kwargs.update({'num_workers': 1, 'pin_memory': True} if use_cuda else {})
+    return DataLoader(**kwargs)
