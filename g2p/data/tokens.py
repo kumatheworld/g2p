ed9768aa2bd1bf9a1f1bfd5c049c8cc445d28afb
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-09-29 20:10:45 +0900

    Add tokens.py

---
+++ g2p/data/tokens.py
@@ -0,0 +1,35 @@
+import string
+import csv
+import torch
+import cmudict
+
+class DoubleBets:
+    class SingleBet:
+        def __init__(self, token_list):
+            self.pad_idx, self.st_idx, self.ed_idx = range(3)
+            pad, st, ed = '=<>'
+            self.i2t = [pad, st, ed] + token_list
+            self.t2i = {t: i for i, t in enumerate(self.i2t)}
+
+        def __len__(self):
+            return len(self.i2t)
+
+        def iseq2tseq(self, iseq):
+            return [self.i2t[i] for i in iseq]
+
+        def tseq2iseq(self, tseq, wrap_st=True, wrap_ed=True):
+            return torch.LongTensor(
+                ([self.st_idx] if wrap_st else []) \
+                + ([self.t2i[t] for t in tseq]) \
+                + [self.st_idx] if wrap_ed else []
+            )
+
+    alphabet = SingleBet(list("'-." + string.ascii_lowercase))
+    arpabet = SingleBet(cmudict.symbols())
+
+    with open('g2p/data/arpabet2ipa.csv') as f:
+        arpabet2ipa = dict(csv.reader(f, delimiter='\t'))
+
+    @classmethod
+    def arpabetSeq2ipaString(cls, arpabet_seq):
+        return ''.join(cls.arpabet2ipa[arpa] for arpa in arpabet_seq)
