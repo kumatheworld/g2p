b6b2cf2228ddebee23702bed0abe77e57f890b4a
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-10-01 23:35:49 +0900

    pad_sequence(), not pack_sequence()

--- g2p/data/load.py
+++ g2p/data/load.py
@@ -1,13 +1,12 @@
-from torch.nn.utils.rnn import pack_sequence
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader
 
-def _pack_data_and_label(batch):
+def _pad_data_and_label(batch):
     data, label = zip(*batch)
-    return pack_sequence(data, enforce_sorted=False), \
-           pack_sequence(label, enforce_sorted=False)
+    return pad_sequence(data), pad_sequence(label)
 
 def get_loader(dataset, batch_size, use_cuda):
     kwargs = {'dataset': dataset, 'batch_size': batch_size,
-              'collate_fn': _pack_data_and_label}
+              'collate_fn': _pad_data_and_label}
     kwargs.update({'num_workers': 1, 'pin_memory': True} if use_cuda else {})
     return DataLoader(**kwargs)
