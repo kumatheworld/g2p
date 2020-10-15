ebba51aeaec9c995df4a7a9edb541dfc5d428580
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-10-15 19:59:27 +0900

    Rename local variable label -> label_seq

--- g2p/evaluation/evaluate.py
+++ g2p/evaluation/evaluate.py
@@ -1,8 +1,8 @@
 import numpy as np
 from g2p.data import DoubleBets
 
-def mean_score(eval_func, pred, label):
+def mean_score(eval_func, pred, label_seq):
     return np.mean([
         eval_func(p, DoubleBets.arpabet.unwrap_iseq(l))
-        for p, l in zip(pred, label.t())
+        for p, l in zip(pred, label_seq.t())
     ])
