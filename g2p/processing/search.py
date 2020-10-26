0123bd04cb1e2b011cad4bfa4129fca1602784e5
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-10-26 22:51:33 +0900

    Add BeamSearchLike()

--- g2p/processing/search.py
+++ g2p/processing/search.py
@@ -1,3 +1,8 @@
+from collections import namedtuple
+from heapq import nlargest
+
+Track = namedtuple('Track', 'len idx state prob_sum hist')
+
 class GreedySearch():
     def __init__(self, limit, st_idx=1, ed_idx=2):
         self.limit = limit
@@ -15,3 +20,39 @@ class GreedySearch():
                 break
             history.append(idx)
         return history
+
+class BeamSearchLike():
+    def __init__(self, beam_size, limit, st_idx=1, ed_idx=2):
+        self.beam_size = beam_size
+        self.limit = limit
+        self.st_idx = st_idx
+        self.ed_idx = ed_idx
+
+    def _select_topk(self, tracks):
+        return nlargest(self.beam_size, tracks,
+                        lambda tr: tr.prob_sum / tr.len)
+
+    def __call__(self, f, state_init):
+        tracks = [Track(0, self.st_idx, state_init, 0, [])]
+        tracks_done = []
+        for _ in range(self.limit):
+            tracks_new = []
+            for track in tracks:
+                prob, state = f(track.idx, track.state)
+                for b in zip(*prob.topk(self.beam_size)):
+                    idx = b[1].item()
+                    hist = track.hist.copy()
+                    if idx != self.ed_idx:
+                        hist.append(idx)
+                    tracks_new.append(Track(track.len + 1, idx, state,
+                                            track.prob_sum + b[0].item(),
+                                            hist))
+            tracks = []
+            for track in self._select_topk(tracks_new):
+                (tracks_done if track.idx == self.ed_idx else tracks).append(track)
+            if len(tracks_done) >= self.beam_size:
+                break
+        else:
+            tracks_done.extend(tracks)
+        tracks = self._select_topk(tracks_done)
+        return tracks[0].hist
