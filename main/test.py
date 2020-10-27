faeb091007881c4529bf552f0415961e092c4e07
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-10-28 07:01:25 +0900

    Add test.py

---
+++ main/test.py
@@ -0,0 +1,32 @@
+import argparse
+import torch
+
+from g2p.config import Config
+from g2p.data import DoubleBets
+
+def predict(device, model, search_algo, word):
+    input_seq = DoubleBets.alphabet.tseq2iseq(word).unsqueeze(1).to(device)
+    input_len = torch.LongTensor([input_seq.size(0)]).to(device)
+    with torch.no_grad():
+        output_seq = model((input_seq, input_len), search_algo=search_algo)[0]
+        output_arpaseq = DoubleBets.arpabet.iseq2tseq(output_seq)
+        return DoubleBets.arpabetSeq2ipaString(output_arpaseq)
+
+def test(cfg):
+    model = cfg.MODEL
+    model.eval()
+    print("Model ready, type whatever word you like!")
+
+    while True:
+        word = input()
+        prediction = predict(cfg.DEVICE, model, cfg.SEARCH, word)
+        print(prediction)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='default',
+                        help="YAML file name under configs/")
+    args = parser.parse_args()
+
+    cfg = Config(args.config, train=False)
+    test(cfg)
