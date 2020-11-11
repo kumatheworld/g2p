1081ee9d036506b79902530835ba60c5c1e9c5c0
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-11-11 23:13:31 +0900

    Flatten Seq2seq, deleting inner classes

--- g2p/models/seq2seq.py
+++ g2p/models/seq2seq.py
@@ -4,100 +4,68 @@ from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 from g2p.models.base import Base
 
 class Seq2Seq(Base):
-    class Encoder(nn.Module):
-        def __init__(self, rnn_type, embedding, hidden_size,
-                     num_layers, dropout, bidirectional):
-            super().__init__()
-            _, embed_dim = embedding.weight.size()
-            self.embedding = embedding
-            self.rnn = getattr(nn, rnn_type)(embed_dim, hidden_size,
+    def __init__(self, rnn_type, src_size, enc_embed_dim, hidden_size,
+                 dec_embed_dim, tgt_size, num_layers, dropout, bidirectional):
+        super().__init__(src_size, enc_embed_dim, tgt_size, dec_embed_dim)
+        # common
+        self.num_layers = num_layers
+
+        # encoder
+        self.bidirectional = bidirectional
+        self.enc_rnn = getattr(nn, rnn_type)(enc_embed_dim, hidden_size,
                                              num_layers=num_layers,
                                              dropout=dropout,
                                              bidirectional=bidirectional)
 
-        def forward(self, data):
-            embedded = self.embedding(data.seq)
-            x0 = pack_padded_sequence(embedded, data.len, enforce_sorted=False)
-            _, h = self.rnn(x0)
-            return h
-
-    class Enc2Dec(nn.Module):
-        def __init__(self, num_layers, bidirectional):
-            super().__init__()
-            if bidirectional:
-                class Add2Directions(nn.Module):
-                    def __init__(self):
-                        super().__init__()
-
-                    def forward(self, h):
-                        return h[:num_layers] + h[num_layers:]
-
-                self.enc2dec = Add2Directions()
-            else:
-                self.enc2dec = nn.Identity()
-
-        def forward(self, hidden):
-            return self.enc2dec(hidden)
-
-    class Decoder(nn.Module):
-        def __init__(self, rnn_type, embedding, hidden_size,
-                     num_layers, dropout):
-            super().__init__()
-            tgt_size, embed_dim = embedding.weight.size()
-            self.embedding = nn.Embedding(tgt_size, embed_dim, padding_idx=0)
-            self.rnn = getattr(nn, rnn_type)(embed_dim, hidden_size,
+        # decoder
+        self.dec_rnn = getattr(nn, rnn_type)(dec_embed_dim, hidden_size,
                                              dropout=dropout,
                                              num_layers=num_layers)
-            self.unembedding = nn.Linear(hidden_size, tgt_size - 2)
-            self.loss_func = nn.CrossEntropyLoss(ignore_index=0)
-            self.softmax = nn.Softmax(dim=-1)
+        self.dec_unembed = nn.Linear(hidden_size, tgt_size - 2)
+        self.loss_func = nn.CrossEntropyLoss(ignore_index=0)
+        self.softmax = nn.Softmax(dim=-1)
 
-        def _compute_logits(self, seq, length, h0):
-            embedded = self.embedding(seq)
-            x0 = pack_padded_sequence(embedded, length, enforce_sorted=False)
-            x, h = self.rnn(x0, h0)
-            padded, _ = pad_packed_sequence(x)
-            logits = self.unembedding(padded)
-            return logits, h
+    def _compute_logits(self, seq, length, h0):
+        embedded = self.dec_emb(seq)
+        x0 = pack_padded_sequence(embedded, length, enforce_sorted=False)
+        x, h = self.dec_rnn(x0, h0)
+        padded, _ = pad_packed_sequence(x)
+        logits = self.dec_unembed(padded)
+        return logits, h
 
-        @staticmethod
-        def _prepend_2zeros(out):
-            zeros = torch.zeros(*out.size()[:-1], 2,
-                                dtype=out.dtype, device=out.device)
-            return torch.cat((zeros, out), dim=-1)
+    @staticmethod
+    def _prepend_2zeros(out):
+        zeros = torch.zeros(*out.size()[:-1], 2,
+                            dtype=out.dtype, device=out.device)
+        return torch.cat((zeros, out), dim=-1)
 
-        def _rec_prob_gen(self, idx, hidden):
-            seq = torch.tensor([[idx]], dtype=torch.long, device=hidden.device)
-            length = torch.ones(1, dtype=torch.long, device=hidden.device)
-            logits, hidden_new = self._compute_logits(seq, length, hidden)
-            prob = self.softmax(logits)
-            return self._prepend_2zeros(prob).squeeze(), hidden_new
-
-        def forward(self, h, label, search_algo):
-            if self.training:
-                logits, _ = self._compute_logits(label.seq[:-1],
-                                                 label.len - 1, h)
-                logits_full = self._prepend_2zeros(logits)
-                return self.loss_func(
-                    logits_full.view(-1, logits_full.size(-1)),
-                    label.seq[1:].flatten()
-                )
-            else:
-                return [
-                    search_algo(self._rec_prob_gen, h[:, i:i+1])
-                    for i in range(h.size(1))
-                ]
-
-
-    def __init__(self, rnn_type, src_size, enc_embed_dim, hidden_size,
-                 dec_embed_dim, tgt_size, num_layers, dropout, bidirectional):
-        super().__init__(src_size, enc_embed_dim, tgt_size, dec_embed_dim)
-        self.encoder = self.Encoder(rnn_type, self.enc_emb, hidden_size,
-                                    num_layers, dropout, bidirectional)
-        self.enc2dec = self.Enc2Dec(num_layers, bidirectional)
-        self.decoder = self.Decoder(rnn_type, self.dec_emb, hidden_size,
-                                    num_layers, dropout)
+    def _rec_prob_gen(self, idx, h):
+        seq = torch.tensor([[idx]], dtype=torch.long, device=h.device)
+        length = torch.ones(1, dtype=torch.long, device=h.device)
+        logits, h_next = self._compute_logits(seq, length, h)
+        prob = self.softmax(logits)
+        return self._prepend_2zeros(prob).squeeze(), h_next
 
     def forward(self, data, label=None, search_algo=None):
-        return self.decoder(self.enc2dec(self.encoder(data)),
-                            label, search_algo)
+        # encoder
+        embedded = self.enc_emb(data.seq)
+        x0 = pack_padded_sequence(embedded, data.len, enforce_sorted=False)
+        _, h = self.enc_rnn(x0)
+
+        # enc2dec
+        if self.bidirectional:
+            h = h[:self.num_layers] + h[self.num_layers:]
+
+        # decoder
+        if self.training:
+            logits, _ = self._compute_logits(label.seq[:-1], label.len - 1, h)
+            logits_full = self._prepend_2zeros(logits)
+            return self.loss_func(
+                logits_full.view(-1, logits_full.size(-1)),
+                label.seq[1:].flatten()
+            )
+        else:
+            return [
+                search_algo(self._rec_prob_gen, h[:, i:i+1])
+                for i in range(h.size(1))
+            ]
